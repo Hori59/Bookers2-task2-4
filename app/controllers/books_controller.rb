1e62bfae622de8512a6e5ba2d63db4507140b81e
vagrant <vagrant@localhost.localdomain>
2020-06-10 09:51:15 +0000

    [Finish]task2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,24 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :baria_user, only: [:edit, :update, :destroy]
 
   def show
+    @newbook = Book.new
   	@book = Book.find(params[:id])
+    @user = User.find(@book.user_id)
+    @books = @user.books
   end
 
   def index
+    @newbook = Book.new
   	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
   end
 
   def create
-  	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
-  	if @book.save #入力されたデータをdbに保存する。
-  		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
+  	@newbook = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+    @newbook.user_id = current_user.id
+  	if @newbook.save #入力されたデータをdbに保存する。
+  		redirect_to @newbook, notice: "successfully created book!"#保存された場合の移動先を指定。
   	else
   		@books = Book.all
   		render 'index'
@@ -22,8 +29,6 @@ class BooksController < ApplicationController
   	@book = Book.find(params[:id])
   end
 
-
-
   def update
   	@book = Book.find(params[:id])
   	if @book.update(book_params)
@@ -33,16 +38,24 @@ class BooksController < ApplicationController
   	end
   end
 
-  def delete
+  def destroy
   	@book = Book.find(params[:id])
-  	@book.destoy
+  	@book.destroy
   	redirect_to books_path, notice: "successfully delete book!"
   end
 
   private
 
   def book_params
-  	params.require(:book).permit(:title)
+  	params.require(:book).permit(:title, :body)
   end
+
+  #url直接防止メソッドを自己定義してbefore_actionで発動。
+   def baria_user
+    book = Book.find(params[:id])
+    unless book.user_id == current_user.id
+      redirect_to books_path
+    end
+   end
 
 end
